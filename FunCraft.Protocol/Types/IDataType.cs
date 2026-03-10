e447651335d3f8eefece9e2c2f132c399afd4d18
Evangeder <Evangeder@gmail.com>
2026-03-10 04:43:14 +0100

    Fixed tabulation in IDataType

--- FunCraft.Protocol/Types/IDataType.cs
+++ FunCraft.Protocol/Types/IDataType.cs
@@ -3,7 +3,7 @@
 namespace FunCraft.Protocol.Types
 {
     public interface IDataType<TSelf, TValue>
-     where TSelf : IDataType<TSelf, TValue>
+        where TSelf : IDataType<TSelf, TValue>
     {
         static abstract int Write(Span<byte> destination, TValue value);
         static abstract bool TryRead(ref SequenceReader<byte> reader, out TValue value);
